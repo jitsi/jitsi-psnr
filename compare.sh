55941c5dea98e844b529df4f878cb23ef63f93bc
George Politis <gp@jitsi.org>
2019-03-05 10:51:50 +0000

    Adds failure handling and resize for PSNR compare.

--- compare.sh
+++ compare.sh
@@ -34,10 +34,32 @@ fi
 mkdir -p "$DIFFERENCES"
 echo "frameno,seqno,psnr" > "$DATA"
 for OUTPUT_FRAME in "$OUTPUT_FRAMES"/*; do
+  # Find the corresponding input frame.
   FRAMENO=`basename "$OUTPUT_FRAME" .png`
-  SEQNO=`dmtxread -n "$OUTPUT_FRAME" --x-range-max 50 --y-range-min 650`
-  INPUT_FRAME="$INPUT_FRAMES/$SEQNO.png"
-  DIFFERENCE="$DIFFERENCES/$SEQNO.png"
-  PSNR=`compare "$OUTPUT_FRAME" "$INPUT_FRAME" -metric PSNR "$DIFFERENCE" 2>&1 || true`
-  echo $FRAMENO,$SEQNO,$PSNR | tee -a "$DATA"
+  SEQNO=`dmtxread -n "$OUTPUT_FRAME" --x-range-max 50 --y-range-min 650 || echo -1`
+  if [ $SEQNO == -1 ]; then
+    echo $FRAMENO,-1,-1 | tee -a "$DATA"
+  else
+    INPUT_FRAME="$INPUT_FRAMES/$SEQNO.png"
+
+    # Identify the input frame and potentially resize the output frame to the
+    # input frame geometry before comparing.
+    INPUT_GEOMETRY=`identify "$INPUT_FRAME" | rev | cut -d' ' -f7 | rev`
+    OUTPUT_GEOMETRY=`identify "$OUTPUT_FRAME" | rev | cut -d' ' -f7 | rev`
+    if [ $INPUT_GEOMETRY != $OUTPUT_GEOMETRY ]; then
+      RESIZED_FRAMES=target/"$WORKING_DIR"/$INPUT_GEOMETRY
+      mkdir -p "$RESIZED_FRAMES"
+      OUTPUT_FRAME_RESIZED="$RESIZED_FRAMES/$FRAMENO.png"
+      if [ ! -f "$OUTPUT_FRAME_RESIZED" ]; then
+        # For some bizzare reason Windows captures 1376x776, which is different
+        # than the native screen resolution (1366x768).
+        convert "$OUTPUT_FRAME" -resize ${INPUT_GEOMETRY}! "$OUTPUT_FRAME_RESIZED"
+      fi
+      OUTPUT_FRAME="$OUTPUT_FRAME_RESIZED"
+    fi
+
+    DIFFERENCE="$DIFFERENCES/$FRAMENO.png"
+    PSNR=`compare "$OUTPUT_FRAME" "$INPUT_FRAME" -metric PSNR "$DIFFERENCE" 2>&1 || true`
+    echo $FRAMENO,$SEQNO,$PSNR | tee -a "$DATA"
+  fi
 done
