d702289a799d8e21d7f789a2ebcb4d82bd0ee4cf
George Politis <gp@jitsi.org>
2019-03-14 13:22:17 +0000

    Adds grab and mm-link scripts.

---
+++ ffmpeg-x11grab.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Usage example:
+# timeout 150s ffmpeg-x11grab.sh Videos/Captures/ATT-LTE-driving-2016_p2p_meet_`date +%s`.mp4
+ffmpeg -loglevel quiet -video_size 1280x720 -framerate 30 -f x11grab -i :0.0+nomouse -c:v libx264 -crf 0 -preset ultrafast "$1"
