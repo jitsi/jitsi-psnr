8a2d16da080bcecc12269d39c5996daca6abd1aa
George Politis <gp@jitsi.org>
2019-03-15 14:29:56 +0000

    Updates ffmpeg-expand.sh

--- ffmpeg-expand.sh
+++ ffmpeg-expand.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INPUT="$1"
-FRAMES="`dirname \"$INPUT\"`/`basename \"$INPUT\" .mp4`"
+FRAMES="`dirname \"$INPUT\"`/`basename \"$INPUT\" | cut -d. -f1`"
 
 mkdir -p "$FRAMES"
 if [ -z "`ls -A \"$FRAMES\"`" ]
