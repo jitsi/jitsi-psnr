d2f4860a4d5966fb522e13b2eaa1778f79085be6
George Politis <gp@jitsi.org>
2019-03-15 12:49:53 +0000

    analyze.py: Compare PSNR plot

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
@@ -9,48 +9,62 @@ import sys
 FREQUENCY = 30/1000
 PERIOD_MS = 1/FREQUENCY
 
-filepath = sys.argv[1]
-df = pd.read_csv(filepath, index_col='frame_num', delimiter=' ')
+def plot_file(filepath, ax_psnr, ax_period):
+    df = pd.read_csv(filepath, index_col='frame_num', delimiter=' ')
 
-frame_num_start = df.index.min()
-frame_num_end = len(df) + frame_num_start - 1
+    frame_num_start = df.index.min()
+    frame_num_end = len(df) + frame_num_start - 1
 
-instants = []
-periods = []
+    instants = []
+    periods = []
 
-# Compare each frame from 1 to n - 1 with the next one computing how long
-# we stayed on a particular frame along the way.
-current_period = PERIOD_MS
-current_instant = 0
-for frame_num in range(frame_num_start, frame_num_end):
-    instants.append(current_instant)
-    current_instant += PERIOD_MS
+    # Compare each frame from 1 to n - 1 with the next one computing how long
+    # we stayed on a particular frame along the way.
+    current_period = PERIOD_MS
+    current_instant = 0
+    for frame_num in range(frame_num_start, frame_num_end):
+        instants.append(current_instant)
+        current_instant += PERIOD_MS
 
-    # Compare frame i with i + 1 to see if the frame has changed.
-    cur_seq_num = df.loc[frame_num, 'sequence_num']
-    next_seq_num = df.loc[frame_num + 1, 'sequence_num']
-    if cur_seq_num < 1 or next_seq_num < 1:
-        # we failed to read the sequence number of either the current or the
-        # next frame.
-        periods.append(current_period)
-        current_period = PERIOD_MS
-    elif cur_seq_num != next_seq_num:
-        periods.append(current_period)
-        current_period = PERIOD_MS
-    else:
-        current_period = current_period + PERIOD_MS
-        periods.append(None)
+        # Compare frame i with i + 1 to see if the frame has changed.
+        cur_seq_num = df.loc[frame_num, 'sequence_num']
+        next_seq_num = df.loc[frame_num + 1, 'sequence_num']
+        if cur_seq_num < 1 or next_seq_num < 1:
+            # we failed to read the sequence number of either the current or the
+            # next frame.
+            periods.append(current_period)
+            current_period = PERIOD_MS
+        elif cur_seq_num != next_seq_num:
+            periods.append(current_period)
+            current_period = PERIOD_MS
+        else:
+            current_period = current_period + PERIOD_MS
+            periods.append(None)
 
-# This is for the last frame, we can't check whether it's changed or not.
-periods.append(None)
-instants.append(None)
+    # This is for the last frame, we can't check whether it's changed or not.
+    periods.append(None)
+    instants.append(None)
 
-df['time'] = instants
-df['period'] = periods
+    df['time'] = instants
+    df['period'] = periods
 
-stats = open(filepath.replace('csv', 'txt'), 'w')
-stats.write(df.describe().to_string())
-stats.close()
+    #df = df[df['psnr'] > -1]
+    ax_psnr.plot(df['time'], df['psnr'])
+    ax_period.plot(df['time'], df['period'])
 
-df.plot(x='time',y=['psnr', 'period'], subplots=True, title=filepath)
-plt.savefig(filepath.replace('csv', 'pdf'))
+def main():
+    if len(sys.argv) == 3:
+        filename1 = sys.argv[1]
+        filename2 = sys.argv[2]
+        f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+        plot_file(filename1, ax1, ax3)
+        plot_file(filename2, ax1, ax3)
+        plt.savefig('/home/gpolitis/Videos/compare.pdf')
+    elif len(sys.argv) == 2:
+        filename1 = sys.argv[1]
+        f, (ax1, ax2) = plt.subplots(2, sharex=True)
+        plot_file(filename1, ax1, ax2)
+        plt.savefig(sys.argv[1].replace('csv', 'pdf'))
+
+if __name__ == "__main__":
+    main()
