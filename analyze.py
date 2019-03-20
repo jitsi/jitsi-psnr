3ee2c2cff1703d091d5ceec7fd87fe79391eebb1
George Politis <gp@jitsi.org>
2019-03-20 17:27:23 +0000

    analyze.sh: Refactor

--- analyze.py
+++ analyze.py
@@ -14,35 +14,29 @@ FREEZE_THRESHOLD_MS = 70
 
 def read_csv(filepath):
     df = pd.read_csv(filepath, index_col='frame_num', delimiter=' ')
+    return augment_dataframe(df)
 
-    new_index = pd.Index(list(range(1, df.index.max() + 1)), name="frame_num")
-    df = df.reindex(new_index)
+def augment_dataframe(df):
+    df['next_sequence_num'] = df['sequence_num'].shift(-1)
 
-    frame_num_start = df.index.min()
-    frame_num_end = len(df) + frame_num_start - 1
-
-    instants = []
     periods = []
-
     # Compare each frame from 1 to n - 1 with the next one computing how long
     # we stayed on a particular frame along the way.
     current_period = PERIOD_MS
-    current_instant = 0
-    for frame_num in range(frame_num_start, frame_num_end):
-        instants.append(current_instant)
-        current_instant += PERIOD_MS
-
+    prev_seq_num = None
+    for frame_num, row in df.iterrows():
         # Compare frame i with i + 1 to see if the frame has changed.
-        cur_seq_num = df.loc[frame_num, 'sequence_num']
-        next_seq_num = df.loc[frame_num + 1, 'sequence_num']
-        if cur_seq_num < 1 or next_seq_num < 1:
+        cur_seq_num = row['sequence_num']
+        next_seq_num = row['next_sequence_num']
+        if math.isnan(cur_seq_num) or cur_seq_num < 1 or next_seq_num < 1:
             raise Exception('Illegal sequence number')
 
-        if math.isnan(cur_seq_num) or math.isnan(next_seq_num):
-            # we failed to read the sequence number of either the current or the
-            # next frame.
-            periods.append(None)
-            current_period = PERIOD_MS
+        if math.isnan(next_seq_num):
+            # This is handling the last frame.
+            if cur_seq_num != prev_seq_num:
+                periods.append(PERIOD_MS)
+            else:
+                periods.append(current_period)
         elif cur_seq_num != next_seq_num:
             periods.append(current_period)
             current_period = PERIOD_MS
@@ -50,72 +44,89 @@ def read_csv(filepath):
             current_period = current_period + PERIOD_MS
             periods.append(None)
 
-    # This is for the last frame.
-    if cur_seq_num != next_seq_num:
-        periods.append(PERIOD_MS)
-    else:
-        periods.append(current_period)
-    instants.append(current_instant + PERIOD_MS)
+        prev_seq_num = cur_seq_num
 
-    df['time'] = instants
     df['period'] = periods
-    df = df.reset_index().set_index('time')
+    df = df.drop('next_sequence_num', axis=1) # drop helper column
+
     return df
 
-def plot_file(df, ax_psnr, ax_period):
-    ax_psnr.plot(df.index, df['psnr'])
-    ax_period.plot(df.index, df['period'])
+def plot_dataframe(df, label, ax_psnr, ax_period):
+    ax_psnr.plot(df.index * PERIOD_MS, df['psnr'], label=label)
+    ax_period.plot(df.index * PERIOD_MS, df['period'], label=label)
 
-def df_list_align(df_list):
+def align_dataframes(df_list):
     new_df_list = []
-    min_end = min(map(lambda df: df.frame_num.max(), df_list))
-    max_start = max(map(lambda df: df[df.psnr.notnull()].frame_num.min(), df_list))
+    # The end frame corresponds to the instant when the experiment ends (because
+    # mahimahi is run with TIMEOUT(1)). So we can align the dataframes by the
+    # end frame. The start frame can correspond to different instants depending
+    # the time it takes for the first media to appear.
+    min_end = min(map(lambda df: df.index.max(), df_list))
+    max_start = max(map(lambda df: df[df.psnr.notnull()].index.min(), df_list))
     for df in df_list:
-        end = df.frame_num.max()
-        df = df[df.frame_num >= max_start]
+        end = df.index.max()
+        df = df[df.index >= max_start]
         df = df.shift(min_end - end)
+        df = df[df.psnr.notnull()]
         new_df_list.append(df)
 
     return new_df_list
 
+def plot_command():
+    if len(sys.argv) >= 4:
+        f, (ax1, ax2) = plt.subplots(2, sharex=True)
+        commonprefix = None
+        df_list = []
+        file_list = []
+        for i in range(2, len(sys.argv)):
+            filename = sys.argv[i]
+            if commonprefix is None:
+                commonprefix = filename
+            else:
+                commonprefix = os.path.commonprefix([filename, commonprefix])
+            df_list.append(read_csv(filename))
+            file_list.append(filename)
+
+        df_list = align_dataframes(df_list)
+        for i in range(0, len(df_list)):
+            label = file_list[i][len(commonprefix):-4]
+            plot_dataframe(df_list[i], label, ax1, ax2)
+
+        ax2.legend()
+        plt.savefig(commonprefix.rstrip('_') + '.png')
+    elif len(sys.argv) == 3:
+        filename1 = sys.argv[2]
+        df1 = read_csv(filename1)
+        f, (ax1, ax2) = plt.subplots(2, sharex=True)
+        plot_dataframe(df1, None, ax1, ax2)
+        ax2.legend()
+        plt.savefig(sys.argv[2].replace('csv', 'png'))
+
+def describe_command():
+    args_len = len(sys.argv)
+    df_list = []
+    file_list = []
+    for i in range(2, args_len):
+        filename1 = sys.argv[i]
+        df_list.append(read_csv(filename1))
+        file_list.append(filename1)
+
+    df_list = align_dataframes(df_list)
+    for i in range(0, len(df_list)):
+        print(file_list[i])
+        df1 = df_list[i]
+        print(df1.describe())
+        freeze_duration = df1[df1['period'] > FREEZE_THRESHOLD_MS]['period'].sum()
+        total_duration = (df1.index.max() - df1.index.min()) * PERIOD_MS
+        print('Total duration: ' + str(total_duration))
+        print('Freeze duration: ' + str(freeze_duration))
+        print('Freeze percentage: ' + str(freeze_duration / total_duration))
+
 def main():
     if sys.argv[1] == "plot":
-        if len(sys.argv) >= 4:
-            f, (ax1, ax2) = plt.subplots(2, sharex=True)
-            commonprefix = None
-            df_list = []
-            for i in range(2, len(sys.argv)):
-                filename = sys.argv[i]
-                if commonprefix is None:
-                    commonprefix = filename
-                else:
-                    commonprefix = os.path.commonprefix([filename, commonprefix])
-                df_list.append(read_csv(filename))
-
-            for df in df_list_align(df_list):
-                plot_file(df, ax1, ax2)
-
-            commonprefix = commonprefix.rstrip('_')
-            plt.savefig(commonprefix + '.png')
-        elif len(sys.argv) == 3:
-            filename1 = sys.argv[2]
-            df1 = read_csv(filename1)
-            f, (ax1, ax2) = plt.subplots(2, sharex=True)
-            plot_file(df1, ax1, ax2)
-            ax1.legend()
-            plt.savefig(sys.argv[2].replace('csv', 'png'))
+        plot_command()
     elif sys.argv[1] == "describe":
-        args_len = len(sys.argv)
-        df_list = []
-        for i in range(2, args_len):
-            filename1 = sys.argv[i]
-            df_list.append(read_csv(filename1))
-
-        for df1 in df_list_align(df_list):
-            print(df1.describe())
-            freeze_duration = df1[df1['period'] > FREEZE_THRESHOLD_MS]['period'].sum()
-            total_duration = df1.index.max()
-            print('Freeze percentage: ' + str(freeze_duration / total_duration))
+        describe_command()
     elif len(sys.argv) == 2:
         filename1 = sys.argv[1]
         df1 = read_csv(filename1)
