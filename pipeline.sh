c704bab083a5d81352df496e3cab01c8748851e8
George Politis <gp@jitsi.org>
2019-04-04 16:02:05 +0100

    Adds a pipeline.sh script.

---
+++ pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for video in ~/Videos/Captures/att*.mp4; do
+  video_dir=`dirname $video`/`basename $video .mp4`
+  mkdir $video_dir
+  ffmpeg -i $video -f image2 $video_dir/%d.png
+  ./map.sh $video_dir
+  rm -rf $video_dir
+done
